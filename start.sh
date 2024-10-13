23847fee9a5197c766129a7181d7de3d48e6f980
Paul Truong <paul@paultruong.dev>
2024-10-13 15:45:03 +1100

    Initial commit, includes a starter plugin and theme

---
+++ start.sh
@@ -0,0 +1,15 @@
+ddev start
+
+ddev wp core download --skip-content
+
+ddev wp core install --url='$DDEV_PRIMARY_URL' --title='pt-wp-starter' --admin_user=admin --admin_email=admin@example.com --prompt=admin_password
+
+ddev composer update
+
+ddev composer install
+
+# Add redis location for the Redis Object Cache plugin
+ddev wp config set WP_REDIS_CLIENT "redis"
+
+# Launch the WordPress admin dashboard in the browser
+ddev launch wp-admin/
