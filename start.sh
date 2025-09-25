a1a405d232027312b61f03bf2eb4b591da375ff8
Paul Truong <paul@paultruong.dev>
2025-09-25 11:27:05 +1000

    added generatepress child theme starter, added extra setup to start.sh file

--- start.sh
+++ start.sh
@@ -2,11 +2,21 @@ ddev start
 
 ddev wp core download --skip-content
 
-ddev wp core install --url='$DDEV_PRIMARY_URL' --title='eTools' --admin_user=admin --admin_email=admin@example.com --prompt=admin_password
+ddev wp core install --url='$DDEV_PRIMARY_URL' --title='Good Site Studios' --admin_user=ptsiteboss --admin_email=paul@paultruong.dev --prompt=admin_password
 
 ddev composer update
 
+# Install composer dependencies including plugins for the project as a whole
 ddev composer install
+
+# Change directory in the starter plugin
+cd wp-content/plugins/ptd-starter-plugin
+
+# Install composer dependencies in the starter plugin
+composer install
+
+# Install npm dependencies in the starter plugin
+npm install
 
 # Add redis location for the Redis Object Cache plugin
 ddev wp config set WP_REDIS_CLIENT "redis" 
